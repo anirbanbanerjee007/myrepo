b659a882df28af7d1db08b28940f0919cab48929
Anirban Banerjee <abanerjee763@gmail.com>
2020-06-05 04:03:41 +0530

    commit on master

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
 import EditProfile from "./components/profile-forms/EditProfile";
 import PrivateRoute from "./components/routing/privateRoute";
 // Redux
@@ -46,6 +48,16 @@ const App = () => {
                 exact
                 path='/edit-profile'
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
               />
             </Switch>
           </section>
